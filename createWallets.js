b790ee2e068a3c2d51371361738a02775390776b
Juan Tapia <juant72@gmail.com>
2022-03-10 12:11:55 -0300

    New accounts

--- createWallets.js
+++ createWallets.js
@@ -3,9 +3,9 @@ var Accounts = require('web3-eth-accounts');
 var accounts = new Accounts();
 
 let new_account;
-for (let i = 0; i < 5; i++) {
+console.log('address,private_key'); 
+for (let i = 0; i <1000; i++) {
   new_account = accounts.create();
-  console.log({
-    'private_key': new_account['privateKey'],
-    'address': new_account['address']});
+//   console.log({'private_key': new_account['privateKey'],    'address': new_account['address']});
+console.log( new_account['address'] + ',' +new_account['privateKey']);
 }
